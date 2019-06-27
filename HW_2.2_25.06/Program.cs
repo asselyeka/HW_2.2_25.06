7213ab738f1431c2a99da0fad31c0329431bb174
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 23:58:45 +0600

    Добавьте файлы проекта.

---
+++ HW_2.2_25.06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2.	Вывести на экран числа 5, 10 и 21 одно под другим.
+
+namespace HW_2._2_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 5;
+            var y = 10;
+            var z = 21;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
